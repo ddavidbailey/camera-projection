bce2ee2c3bfd54d5527567b5b8c379c673c430e5
Eckhoee <8399460+Eckhoee@users.noreply.github.com>
2026-05-20 15:51:42 -0400

    init commit

---
+++ utils/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
+import { Pool } from "pg";
+
+export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL as string,
+  database: new Pool({
+    connectionString: process.env.DATABASE_URL as string,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    dropbox: {
+      clientId: process.env.DROPBOX_CLIENT_ID as string,
+      clientSecret: process.env.DROPBOX_CLIENT_SECRET as string,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  plugins: [nextCookies()],
+});
