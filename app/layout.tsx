609aee4f24d21740545a5c6c9a96e49e6a7e98db
Eckhoee <8399460+Eckhoee@users.noreply.github.com>
2026-05-24 21:57:47 -0400

    Solved ui issue with tailwind not rendering actual darkmode

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} ${instrumentSerif.variable} ${interTight.variable} ${jetbrainsMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col font-ui antialiased [text-rendering:optimizeLegibility]">{children}</body>
     </html>
   );
 }
