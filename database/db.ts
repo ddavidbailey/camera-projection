5cee0821545f146d2cd5a3cfc4789037de356721
Eckhoee <8399460+Eckhoee@users.noreply.github.com>
2026-05-25 23:27:14 -0400

    created and migrated schemas for the user-integrations and sharable links

---
+++ database/db.ts
@@ -0,0 +1,8 @@
+import { Pool } from "pg";
+
+let _pool: Pool | undefined;
+
+export function getPool(): Pool {
+  if (!_pool) _pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  return _pool;
+}
